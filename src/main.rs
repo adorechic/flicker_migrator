be9434e412a3e2bfc883cfd463c6d48d2b9048f3
adorechic <adorechic@gmail.com>
2018-07-22 11:28:28 +0900

    Get request with flickr api_key

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 extern crate reqwest;
 
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    let body = reqwest::get("https://www.rust-lang.org").unwrap().text();
+    let mut file = File::open("./api_key").expect("File not found");
+    let mut api_key = String::new();
+    file.read_to_string(&mut api_key).expect("Read error");
+    let uri = format!("https://api.flickr.com/services/rest/?method=flickr.test.echo&name=value&api_key={}", api_key);
+    let body = reqwest::get(&uri).unwrap().text();
 
     println!("body = {:?}", body);
 }

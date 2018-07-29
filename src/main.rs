6c54cd99fc1cb19c4dc522f7104067278a5654e7
adorechic <adorechic@gmail.com>
2018-07-29 17:01:57 +0900

    Use url for readability

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate reqwest;
 use std::fs::File;
 use std::io::prelude::*;
 use std::env;
+use reqwest::Url;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -21,8 +22,15 @@ fn fetch_photos() {
     let mut file = File::open("./api_key").expect("File not found");
     let mut api_key = String::new();
     file.read_to_string(&mut api_key).expect("Read error");
-    let uri = format!("https://api.flickr.com/services/rest/?method=flickr.test.echo&name=value&api_key={}", api_key);
-    let body = reqwest::get(&uri).unwrap().text();
+    let url = Url::parse_with_params(
+        "https://api.flickr.com/services/rest/",
+        &[
+            ("method", "flickr.test.echo"),
+            ("name", "value"),
+            ("api_key", &api_key)
+        ]
+    ).unwrap();
+    let body = reqwest::get(url).unwrap().text();
 
     println!("body = {:?}", body);
 }
